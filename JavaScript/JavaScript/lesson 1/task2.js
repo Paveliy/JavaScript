471c40c18a24a83f79c20261039bfc491061d82c
9Pasha <91071679+Paveliy@users.noreply.github.com>
2022-05-17 23:21:17 +0300

    Add files via upload

---
+++ JavaScript/JavaScript/lesson 1/task2.js
@@ -0,0 +1,11 @@
+// input data
+
+const width = 70;
+const height = 40;
+
+//square
+
+let square = width * height;
+
+//output data
+console.log(`Площадь поямоугольника шириной 70см и высотой 40см равна ${square} см^2`);
---
+++ JavaScript/JavaScript/lesson 1/task5.js
@@ -0,0 +1,19 @@
+//randomizer
+
+const randomNumber = Math.floor(Math.random() * 100);
+
+// console.log(randomNumber); //for testing code
+
+//solution
+
+switch (true) {
+    case (randomNumber < 20):
+        console.log('randomNumber меньше 20');
+        break;
+    case (randomNumber > 50):
+        console.log('randomNumber больше 50');
+        break;
+    default:
+        console.log('randomNumber больше 20, и меньше 50');
+        break;
+}
